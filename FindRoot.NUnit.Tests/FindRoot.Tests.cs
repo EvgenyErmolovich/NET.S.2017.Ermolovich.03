e2a4dc62bd4ddf30bac1a057595d0f4f84e3f9e9
EvgenyErmolovich <evgeny@192.168.100.2>
2017-07-09 11:00:03 +0300

    Code

---
+++ FindRoot.NUnit.Tests/FindRoot.Tests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+namespace LogicFindRoot.NUnit.Tests
+{
+	[TestFixture()]
+	public class FindRootTests
+	{
+		[TestCase(3, 2, 0.00001, ExpectedResult = 1.73205)]
+		[TestCase(3, 2, 0.001, ExpectedResult = 1.732)]
+		[TestCase(3, 2, 0.000001, ExpectedResult = 1.732051)]
+		[TestCase(3, 2, 0.0000001, ExpectedResult = 1.7320508)]
+		public double NewtonFormula_PositiveTests(double number, int n, double eps)
+		{
+			return FindRoot.NewtonFormula(number, n, eps);
+		}
+
+		[TestCase(3, -2, 0.0002)]
+		[TestCase(-3, 2, 0.00002)]
+		[TestCase(-3, -2, 0.00001)]
+		public void NewTonFormula_ArgumentException(double number, int n, double eps)
+		{
+			Assert.Throws<ArgumentException>(() => FindRoot.NewtonFormula(number, n, eps));
+		}
+
+		[TestCase(3, 2, -0.0002)]
+		[TestCase(3, 2, 1.00002)]
+		public void NewTonFormula_ArgumentOutOfRangeException(double number, int n, double eps)
+		{
+			Assert.Throws<ArgumentOutOfRangeException>(() => FindRoot.NewtonFormula(number, n, eps));
+		}
+	}
+}
---
+++ LogicFindRoot/FindRoot.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace LogicFindRoot
+{
+	public static class FindRoot
+	{
+		public static double NewtonFormula(double number, int n, double eps)
+		{
+			ValidInput(number, n, eps);
+			int count = 0;
+			double temp = eps;
+			while (temp < 1) 
+			{
+				temp *= 10;
+				count++;
+        	}
+			double x1 = number / n;
+			double x2 = 1.0 / n * ((n - 1) * x1 + number / Math.Pow(x1, n - 1));
+            while (Math.Abs(x1 - x2) > eps) 
+            {
+                x1 = x2;
+                x2 = 1.0 / n * ((n - 1) * x1 + number / Math.Pow(x1, n - 1));
+            }
+			return Math.Round(x2,count);
+		}
+
+		private static void ValidInput(double number, int n, double eps)
+		{
+			if (number <= 0 || n <= 0) throw new ArgumentException("Number must be > 0!");
+			if (eps <= 0 || eps >= 1) throw new ArgumentOutOfRangeException("0 < eps < 1!");
+		}
+	}
+}
